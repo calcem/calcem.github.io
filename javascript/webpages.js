8d7b94d7f2cadd42a9e244f96c31a8b2d01fb3c7
Mario Calce <mariocalce12@gmail.com>
2020-06-28 22:31:51 -0400

    brought back old

--- javascript/webpages.js
+++ javascript/webpages.js
@@ -1,29 +1,44 @@
-function changeDivDisplayed(divId)
+function hideAndDisplay(divId)
 {
-	switch(divId)
-	{
-	      case: "about"
-		displayBlock("about");
-		hideDiv("socials");
-	        hideDiv("projects");
-		break;
-	
-	      case: "socials"
-		displayBlock("socials");
-		hideDiv("about");
-		hideDiv("projects");
-		break;
-	
-	      case: "projects"
-		displayBlock("projects");
-		hideDiv("about");
-		hideDiv("socials");
-		break;
-	     
-	     default:
-		diplayBlock("about");
-		hideDiv("projects");
-		hideDiv("socials");
-		break;
+	var div = document.getElementById(divId);
+	switch(div){
+		case "bio":
+			setDisplayBlock("bio");
+			setDisplayNone("work");
+			setDisplayNone("social");
+		 break;
+		case "work":
+			setDisplayGrid("work");
+			setDisplayNone("bio");
+			setDisplayNone("socials");
+			break;
+		case "social":
+			setDisplayGrid("social");
+			setDisplayNone("work");
+			setDisplayNone("bio");
+			break;
+		default:
+			setDisplayBlock("bio");
+			setDisplayNone("work");
+			setDisplayNone("social");
 	}
+
+}
+
+function setDisplayBlock(divId)
+{
+	var div = document.getElementById(divId);
+	div.style.display = "block";
+}
+
+function setDisplayGrid(divId)
+{
+	var div = document.getElementById(divId);
+	div.style.display = "grid";
+}
+
+function setDisplayNone(divId)
+{
+	var div = document.getElementById(divId);
+	div.style.display = "none";
 }
